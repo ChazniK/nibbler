6cf9e66ec5f32a898b40968cc9411bd963c07d87
Marco FERNANDES <mafernan@e4r20p3.wethinkcode.co.za>
2018-08-10 18:40:17 +0200

    auto commit

--- LIB1/interface.cpp
+++ LIB1/interface.cpp
@@ -6,19 +6,21 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 13:10:42 by mafernan          #+#    #+#             */
-/*   Updated: 2018/08/08 11:32:47 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/08/10 18:33:09 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Game.hpp"
 #include <iostream>
 
-/*extern "C" void	test(void)
+// this gets called in the main.cpp
+extern "C" void	test(void)
 {
 	std::cout << "start" << std::endl;
 	// Program entry point.
 	Game game; // Creating our game object.
 
+	//game.CreateWindow();
 	while(!game.GetWindow()->IsDone())
 	{
 		// Game loop.
@@ -28,22 +30,5 @@
 		game.RestartClock();
 	}
 	std::cout << "closed" << std::endl;
-}*/
-
-int		main(void)
-{
-	std::cout << "start" << std::endl;
-	// Program entry point.
-	Game game; // Creating our game object.
-
-	while(!game.GetWindow()->IsDone())
-	{
-		// Game loop.
-		game.HandleInput();
-		game.Update();
-		game.Render();
-		game.RestartClock();
-	}
-	std::cout << "closed" << std::endl;
-	return (0);
 }
+
