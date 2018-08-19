7df80fe1708fd67b75b7128fa50688f397c508fc
Marco FERNANDES <mafernan@e4r20p3.wethinkcode.co.za>
2018-08-19 10:01:57 +0200

    cleaned repo for new code

--- LIB2/sdl_check.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-#include "../srcs/Error.hpp"
-
-#define SDL_CHECK(x, msg) if (!(x))                                      \
-    throw Error::RuntimeError(std::string(msg": ") + SDL_GetError())
